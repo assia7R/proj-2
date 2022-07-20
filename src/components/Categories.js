a07ecb2056a7bd1a9a26eb661b87ebf7cd383abc
assia7R <argui076@uottawa.ca>
2022-07-20 19:19:52 -0400

    Add files via upload

---
+++ src/components/Categories.js
@@ -0,0 +1,108 @@
+const Categories = [
+    {
+        id: "1",      
+        category: "Muffins",
+        title: "Salted Caramel Cupcake",
+        price: "2.50$",
+        image: "../assets/cup.jpeg",
+        des:'desSalted Caramel CupcakeSalted Caramel CupcakeSalted Caramel',
+      },
+      {
+      id: "2",      
+      category: "Pastries",
+      title: "Pistachio Cream Puffs",
+      price: "2.75$",
+      image: "../assets/Pistachio.jpeg",
+      des:'desPistachio Cream PuffsPistachio Cream PuffsPistachio Cream Cream',
+    },
+    {
+        category: "Deserts",
+      id: "3",
+      title: "Mini Roses Cheescake",
+      price: "5.99$",
+      image: "../assets/sweet.jpeg",
+      des:'desMini Roses CheescakeMini Roses CheescakeMini Roses CheescakeMini',
+    },
+    
+    {
+      id: "4",
+      title: "Choco-Raspberry Cupcake",
+      category: "Muffins",
+      price: "2.50$",
+      image: "../assets/Raspberry.jpeg",
+      des:'desChoco-Raspberry CupcakeChoco-Raspberry CupcakeChoco- CupcakeChoco',
+
+    },
+    {
+        id: "5",      
+        category: "Deserts",
+        title: "Peach Tart",
+        price: "3.00$",
+        image: "../assets/tart.png",
+        des:'desPeach TartPeach TartPeach TartPeach TartPeach TartPeach',
+      },
+      {
+          category: "Donuts",
+        id: "6",
+        title: "Gingerbread Donut",
+        price: "2.00$",
+        image: "../assets/donut.jpeg",
+        des:'desGingerbread DonutGingerbread DonutGingerbread DonutGingerbread ',
+      },
+      
+      {
+        id: "7",
+        title: "Pinky Cream Cake",
+        category: "Deserts",
+        price: "5.00$",
+        image: "../assets/cake.jpeg",
+        des:'desPinky Cream CakePinky Cream CakePinky Cream CakePinky Cream ',
+      },{
+        id: "8",      
+        category: "Deserts",
+        title: "Choclate Praline Desert",
+        price: "4.99$",
+        image: "../assets/Chocolaterie.jpeg",
+        des: "desChoclate Praline Desert Dese rtDese rtDese rtDes ertDesert ",
+      },
+      {
+          category: "Donuts",
+        id: "9",
+        title: "Cinnamon Pear Donut",
+        price: "2.00$",
+        image: "../assets/Bakedcinnam_pear_donat.jpeg",
+        des: "desCinnamon Pear Donut PearPe arPe arPearPe arPear PearP earPear",
+
+      },
+      
+      {
+        id: "10",
+        title: "Choclate Roses Eclair",
+        category: "Pastries",
+        price: "4.00$",
+        image: "../assets/ec.jpeg",
+        des: "desChoclate Roses Eclair RosesRo sesRos esRosesR osesRose sRoses",
+
+      },
+      {
+        id: "11",      
+        category: "Cookies",
+        title: "Stuffed Choclate Cookies",
+        price: "1.50$",
+        image: "../assets/cookie.jpeg",
+        des: "des Stuffed Choclate Cookies StuffedStu ffedSt uffedStuffed Stuffed",
+
+      },
+      {
+        id: "12",
+        title: "Coffee Eclair",
+        category: "Pastries",
+        price: "4.00$",
+        image: "../assets/Coffee-Eclairs.jpeg",
+        des: "des Coffee Eclair Coffee Coffe eCoffee CoffeeCo ffeeCo ffeeCo",
+
+      },
+      
+  ];
+   
+  export default Categories;
